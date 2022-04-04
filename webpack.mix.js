631ac04db36e2a13450355ccb73eb216968567f0
a33372 <hai.nguyen592000@gmail.com>
2022-04-04 23:07:41 +0700

    Project_MaxShop

---
+++ webpack.mix.js
@@ -0,0 +1,17 @@
+const mix = require("laravel-mix");
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js("resources/js/app.js", "public/js")
+    .postCss("resources/css/app.css", "public/css", [])
+    .sass("public/app/scss/app.scss", "public/app/public/css")
+    .js('public/app/js/app.js', 'public/app/public/js');
